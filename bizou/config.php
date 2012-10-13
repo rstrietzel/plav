bf51bac6ef21de0752fba7ba35946e5dba87ca62
Arian Sanusi <arian@sanusi.de>
2012-10-13 12:16:00 +0200

    removed accidental copy of bizou in subfolder bizou

--- bizou/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('THUMB_SIZE', 100);
-define('DATA_DIR', 'data');
-define('IMAGES_DIR', 'images');
-define('DATA_UMASK', 0002); # 0002: allow group to write for files created in data dir. change to 0022 to be more strict.
-?>
--- bizou/plugins/viewer/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function getImageLink($imageSimplePath)
-{
-	return $GLOBALS['rootUrl']."plugins/viewer/view.php$imageSimplePath";
-}
-?>
