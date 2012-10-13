ea4f9c89914616bd81d651667ed96072e69a07e3
Robert <robert@sup0rpow0r-moeftl.(none)>
2012-10-13 13:51:14 +0200

    Show pictures from a specified dir with galleria, galleria theming

--- show.php
+++ show.php
@@ -3,18 +3,56 @@
 <!doctype html>
 <html>
     <head>
+        <style>
+            #galleria{ width: 100%; height: 100%; background: #000 }
+            html, body {background: #000; width: 100%; height: 100%; padding:0; margin: 0;}
+        </style>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
         <script src="galleria/galleria-1.2.8.min.js"></script>
     </head>
     <body>
+        <div id="buttons">
+            <a href="#" id="slideshow">Slideshow</a>
+            <a href="#" id="fullscreen">Fullscreen</a>
+            <a href="#" id="download">Zur Downloadliste hinzufuegen</a>
+        </div>
         <div id="galleria">
-            <img src="photo1.jpg">
-            <img src="photo2.jpg">
-            <img src="photo3.jpg">
+
+<?php
+//echo serialize("pics/testpics");
+$dir=$_GET['dir'];
+if ($handle = opendir($dir)) {
+    while (false !== ($file = readdir($handle))) {
+        if (stripos($file, "jpg") !== false) {
+            echo "<img src=\"".$dir."/".$file."\">";
+        }
+    }
+    closedir($handle);
+}
+?>
         </div>
         <script>
+
+                
             Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
-            Galleria.run('#galleria');
+
+           Galleria.run('#galleria', {
+                transition: 'fade',
+                responsive: true
+            });
+           Galleria.ready(function() {
+              var gallery = this; // galleria is ready and the gallery is assigned
+              $('#fullscreen').click(function() {
+                gallery.toggleFullscreen(); // toggles the fullscreen
+              });
+            $("#buttons").hover(function(){$(this).fadeTo(1,200);},function(){$(this).fadeTo(500,0.0001);});
+            $(".galleria-thumbnails-container").hover(function(){$(this).fadeTo(1,200);},function(){$(this).fadeTo(500,0.0001);});
+              
+            });
+
+            //Galleria.ready(function() {
+            //   $('#galleria').data('galleria').enterFullscreen();
+            //});
         </script>
     </body>
 </html>
