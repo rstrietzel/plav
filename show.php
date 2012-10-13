211b9748814e6ddceac34266b37a11511de70829
Robert <robert@sup0rpow0r-moeftl.(none)>
2012-10-13 15:37:19 +0200

    added download checkbox, list ist saved in cookie, fullscreen button

--- show.php
+++ show.php
@@ -9,6 +9,30 @@
         </style>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
         <script src="galleria/galleria-1.2.8.min.js"></script>
+        <script type="text/javascript">
+        function setCookie(c_name,value,exdays)
+            {
+            var exdate=new Date();
+            exdate.setDate(exdate.getDate() + exdays);
+            var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+            document.cookie=c_name + "=" + c_value;
+            }
+
+        function getCookie(c_name)
+            {
+            var i,x,y,ARRcookies=document.cookie.split(";");
+            for (i=0;i<ARRcookies.length;i++)
+            {
+              x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+              y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+              x=x.replace(/^\s+|\s+$/g,"");
+              if (x==c_name)
+                {
+                return unescape(y);
+                }
+              }
+            }            
+        </script>
     </head>
     <body>
         <div id="buttons">
@@ -18,21 +42,20 @@
         </div>
         <div id="galleria">
 
-<?php
-//echo serialize("pics/testpics");
-$dir=$_GET['dir'];
-if ($handle = opendir($dir)) {
-    while (false !== ($file = readdir($handle))) {
-        if (stripos($file, "jpg") !== false) {
-            echo "<img src=\"".$dir."/".$file."\">";
+        <?php
+        $dir=$_GET['dir'];
+        if ($handle = opendir($dir)) {
+            while (false !== ($file = readdir($handle))) {
+                if (stripos($file, "jpg") !== false) {
+                    echo "<img src=\"".$dir."/".$file."\">";
+                }
+            }
+            closedir($handle);
         }
-    }
-    closedir($handle);
-}
-?>
+        ?>
         </div>
         <script>
-
+        var downloadlist;
                 
             Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
 
@@ -41,13 +64,55 @@ if ($handle = opendir($dir)) {
                 responsive: true
             });
            Galleria.ready(function() {
-              var gallery = this; // galleria is ready and the gallery is assigned
-              $('#fullscreen').click(function() {
-                gallery.toggleFullscreen(); // toggles the fullscreen
-              });
-            $("#buttons").hover(function(){$(this).fadeTo(1,200);},function(){$(this).fadeTo(500,0.0001);});
-            $(".galleria-thumbnails-container").hover(function(){$(this).fadeTo(1,200);},function(){$(this).fadeTo(500,0.0001);});
-              
+                var gallery = this; // galleria is ready and the gallery is assigned
+                $('#fullscreen').click(function() {
+                        gallery.toggleFullscreen(); // toggles the fullscreen
+                });
+                //autohide buttons and thumblist
+                $("#buttons").hover(function(){$(this).fadeTo(1,200);},function(){$(this).fadeTo(500,0.0001);});
+                $(".galleria-thumbnails-container").hover(function(){$(this).fadeTo(1,200);},function(){$(this).fadeTo(500,0.0001);});
+            //downloadlist refresh
+                this.bind("image", function(e) {
+                    var cookie = getCookie("downloadlist");
+                    if(cookie != null && cookie != ""){
+                        var downloadlist = cookie.split(',');
+                        Galleria.log(downloadlist);
+                        if($.inArray(e.galleriaData.image, downloadlist) != "-1" ){
+                            $('#download').addClass('checked');
+                        }
+                        else {
+                            $('#download').removeClass('checked');
+                        }
+                    }
+
+
+                    Galleria.log(e.galleriaData.image);
+                });
+                $('#download').click(function() {
+                    var e = gallery.getData();
+                    Galleria.log(e);
+                    var cookie = getCookie("downloadlist");
+                    if(cookie != null && cookie != ""){
+                        var downloadlist = cookie.split(',');
+                        if($.inArray(e.image, downloadlist) != "-1"){
+                            downloadlist.splice($.inArray(e.image, downloadlist), 1);
+                            $('#download').removeClass('checked');
+
+                        }
+                        else {
+                            downloadlist.push(e.image);
+                            $('#download').addClass('checked');
+
+                        }
+                    }
+                    else{
+                        var downloadlist = e.image;
+                        $('#download').addClass('checked');
+
+                    }            
+                    setCookie("downloadlist",downloadlist,3);
+
+                });
             });
 
             //Galleria.ready(function() {
