9ad87b30904a89683b791f5d05d9c52753ce5624
Arian Sanusi <arian@sanusi.de>
2012-11-03 15:50:38 +0100

    genWebQual.sh will now deal sanely with whitespace in filenames hopefully

--- genWebQual.sh
+++ genWebQual.sh
@@ -20,14 +20,14 @@ ImagesDir=`$php -r 'include("config.php"); echo IMAGES_DIR;'`
 wq=`$php -r 'include("config.php"); echo WEB_QUALITY;'`
 dest=${destRoot}/${folder#$ImagesDir}
 
-if [ !  -e "$dest" ] ; then mkdir -p $dest ; fi
+if [ !  -e "$dest" ] ; then mkdir -p "$dest" ; fi
 if [ -e "${dest}/.lock" ] ; then
     echo "another instance is running in $dest, exiting"
     exit
 fi
 
-find ${dest} -maxdepth 1 -type f | while read file ; do
-    sfile=${folder}/$(basename $file)
+find "${dest}" -maxdepth 1 -type f | while read file ; do
+    sfile="${folder}/$(basename $file)"
     if [ ! -e "$sfile" ] ; then
         rm "$sfile"
     fi
@@ -35,12 +35,12 @@ done
 touch "${dest}/.lock" 
 if [ $? -ne 0 ] ; then logger -s "plav: could not touch ${dest}/lock" ; exit 1; fi
     
-find $folder -maxdepth 1 | while read item ; do
-    ditem=${destRoot}/${item#$ImagesDir}
-    mimetype=$(file -bi $item)
-    if [ ${mimetype:0:5} == "image" -a ! -e $ditem ] ; then
-        $IMConvert -auto-orient -resize $size -quality $wq $item $ditem
+find "$folder" -maxdepth 1 | while read item ; do
+    ditem="${destRoot}/${item#$ImagesDir}"
+    mimetype=$(file -bi "$item")
+    if [ ${mimetype:0:5} == "image" -a ! -e "$ditem" ] ; then
+        $IMConvert -auto-orient -resize $size -quality "$wq" "$item" "$ditem"
     fi
 done
 
-rm ${dest}/.lock
+rm "${dest}"/.lock
